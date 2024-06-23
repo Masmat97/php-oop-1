4560ec9033e5e1b263109e8a9be07115c5627c31
Masmat97 <matteo.mascio@libero.it>
2024-06-23 19:56:34 +0200

    M1

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<?php
+
+class Movie {
+    public $nome;
+    public $genere;
+    public $anno;
+
+}
+
+$first = new Movie();
+$first->nome = "First";
+$first->genere = "Action";
+$first->anno = 2015;
+
+$second = new Movie();
+$second->nome = "Second";
+$second->genere = "Horror";
+$second->anno = 2010;
+
+$third = new Movie();
+$third->nome = "Third";
+$third->genere = "Adventure";
+$third->anno = 2005;
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +37,11 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <ul>
+    <li><?= "$first->nome $first->genere $first->anno" ?></li>
+    <li><?= "$second->nome $second->genere $second->anno" ?></li>
+    <li><?= "$third->nome $third->genere $third->anno" ?></li>
+
+    </ul>
 </body>
 </html>
